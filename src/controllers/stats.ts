a39b0a18904149da457bd857a53c84021dc9f4eb
Parminder <parminderpalsingh9203@gmail.com>
2024-02-08 01:41:51 -0800

    Changed the lastest transactions structure for the dashboard page

--- src/controllers/stats.ts
+++ src/controllers/stats.ts
@@ -13,187 +13,207 @@ import {
 export const dashboradStats = TryCatch(
   async (req: Request, res: Response, next: NextFunction) => {
     let stats;
-    if (cache.has("admin-dashboard-stats")) {
-      stats = JSON.parse(cache.get("admin-dashboard-stats") as string);
-    } else {
-      const today = new Date();
-      const lastSixMonths = new Date();
-      lastSixMonths.setMonth(lastSixMonths.getMonth() - 6);
+    // if (cache.has("admin-dashboard-stats")) {
+    //   stats = JSON.parse(cache.get("admin-dashboard-stats") as string);
+    // } else {
+    const today = new Date();
+    const lastSixMonths = new Date();
+    lastSixMonths.setMonth(lastSixMonths.getMonth() - 6);
 
-      // Getting this month and previous month last and end dates
-      const thisMonth = {
-        start: new Date(today.getFullYear(), today.getMonth(), 1),
-        end: today,
-      };
-      const lastMonth = {
-        start: new Date(today.getFullYear(), today.getMonth() - 1, 1),
-        end: new Date(today.getFullYear(), today.getMonth(), 0),
-      };
+    // Getting this month and previous month last and end dates
+    const thisMonth = {
+      start: new Date(today.getFullYear(), today.getMonth(), 1),
+      end: today,
+    };
+    const lastMonth = {
+      start: new Date(today.getFullYear(), today.getMonth() - 1, 1),
+      end: new Date(today.getFullYear(), today.getMonth(), 0),
+    };
 
-      // Getting this month and last month users count
-      const previousMonthUsersPromise = User.find({
-        createdAt: {
-          $gte: lastMonth.start,
-          $lte: lastMonth.end,
-        },
-      });
-      const thisMonthUsersPromise = User.find({
-        createdAt: {
-          $gte: thisMonth.start,
-          $lte: thisMonth.end,
-        },
-      });
+    // Getting this month and last month users count
+    const previousMonthUsersPromise = User.find({
+      createdAt: {
+        $gte: lastMonth.start,
+        $lte: lastMonth.end,
+      },
+    });
+    const thisMonthUsersPromise = User.find({
+      createdAt: {
+        $gte: thisMonth.start,
+        $lte: thisMonth.end,
+      },
+    });
 
-      // Getting this month and last month products count
-      const previousMonthProductsPromise = Product.find({
-        createdAt: {
-          $gte: lastMonth.start,
-          $lte: lastMonth.end,
-        },
-      });
-      const thisMonthProductsPromise = Product.find({
-        createdAt: {
-          $gte: thisMonth.start,
-          $lte: thisMonth.end,
-        },
-      });
-      // Getting this month and last month orders count
-      const previousMonthOrdersPromise = Order.find({
-        createdAt: {
-          $gte: lastMonth.start,
-          $lte: lastMonth.end,
-        },
-      });
-      const thisMonthOrdersPromise = Order.find({
-        createdAt: {
-          $gte: thisMonth.start,
-          $lte: thisMonth.end,
-        },
-      });
-      // Getting previous six months orders users count
-      const lastSixMonthOrdersPromise = Order.find({
-        createdAt: {
-          $gte: lastSixMonths,
-          $lte: today,
-        },
-      });
-      // Getting latest 4 transactions
-      const latestTransactionsPromise = Order.find({})
-        .select(["total", "status", "discount", "orderItems"])
-        .limit(4);
-      const [
-        previousMonthUsers,
-        thisMonthUsers,
-        previousMonthProducts,
-        thisMonthProducts,
-        previousMonthOrders,
-        thisMonthOrders,
-        productsCount,
-        usersCount,
-        allOrders,
-        lastSixMonthOrders,
-        categories,
-        femaleUsersCount,
-        latestTransactions,
-      ] = await Promise.all([
-        previousMonthUsersPromise,
-        thisMonthUsersPromise,
-        previousMonthProductsPromise,
-        thisMonthProductsPromise,
-        previousMonthOrdersPromise,
-        thisMonthOrdersPromise,
-        Product.countDocuments(),
-        User.countDocuments(),
-        Order.find({}).select("total"),
-        lastSixMonthOrdersPromise,
-        Product.distinct("category"),
-        User.countDocuments({ gender: "female" }),
-        latestTransactionsPromise,
-      ]);
+    // Getting this month and last month products count
+    const previousMonthProductsPromise = Product.find({
+      createdAt: {
+        $gte: lastMonth.start,
+        $lte: lastMonth.end,
+      },
+    });
+    const thisMonthProductsPromise = Product.find({
+      createdAt: {
+        $gte: thisMonth.start,
+        $lte: thisMonth.end,
+      },
+    });
+    // Getting this month and last month orders count
+    const previousMonthOrdersPromise = Order.find({
+      createdAt: {
+        $gte: lastMonth.start,
+        $lte: lastMonth.end,
+      },
+    });
+    const thisMonthOrdersPromise = Order.find({
+      createdAt: {
+        $gte: thisMonth.start,
+        $lte: thisMonth.end,
+      },
+    });
+    // Getting previous six months orders users count
+    const lastSixMonthOrdersPromise = Order.find({
+      createdAt: {
+        $gte: lastSixMonths,
+        $lte: today,
+      },
+    });
+    // Getting latest 4 transactions
+    const latestTransactionsPromise = Order.find({})
+      .select(["total", "status", "discount", "orderItems"])
+      .limit(4);
+    const [
+      previousMonthUsers,
+      thisMonthUsers,
+      previousMonthProducts,
+      thisMonthProducts,
+      previousMonthOrders,
+      thisMonthOrders,
+      productsCount,
+      usersCount,
+      allOrders,
+      lastSixMonthOrders,
+      categories,
+      femaleUsersCount,
+      latestTransactions,
+    ] = await Promise.all([
+      previousMonthUsersPromise,
+      thisMonthUsersPromise,
+      previousMonthProductsPromise,
+      thisMonthProductsPromise,
+      previousMonthOrdersPromise,
+      thisMonthOrdersPromise,
+      Product.countDocuments(),
+      User.countDocuments(),
+      Order.find({}).select("total"),
+      lastSixMonthOrdersPromise,
+      Product.distinct("category"),
+      User.countDocuments({ gender: "female" }),
+      latestTransactionsPromise,
+    ]);
 
-      const previousMonthRevenue = previousMonthOrders.reduce(
-        (total, order) => {
-          return total + (order.total || 0);
-        },
-        0
+    const previousMonthRevenue = previousMonthOrders.reduce((total, order) => {
+      return total + (order.total || 0);
+    }, 0);
+    const thisMonthRevenue = thisMonthOrders.reduce(
+      (total, order) => total + (order.total || 0),
+      0
+    );
+    const revenue = allOrders.reduce(
+      (total, order) => total + (order.total || 0),
+      0
+    );
+    const percentageChange = {
+      users: calculatePercentage(
+        thisMonthUsers.length,
+        previousMonthUsers.length
+      ),
+      products: calculatePercentage(
+        thisMonthProducts.length,
+        previousMonthProducts.length
+      ),
+      orders: calculatePercentage(
+        thisMonthOrders.length,
+        previousMonthOrders.length
+      ),
+      revenue: calculatePercentage(thisMonthRevenue, previousMonthRevenue),
+    };
+
+    const count = {
+      revenue,
+      products: productsCount,
+      users: usersCount,
+      orders: allOrders.length,
+    };
+
+    const lastSixMonthTransactions = new Array(6).fill(0);
+    const lastSixMonthRevenue = new Array(6).fill(0);
+
+    // Getting past six months data for the graph on admin dashboard
+    lastSixMonthOrders.forEach((order) => {
+      let createdAt = order.createdAt;
+      let yearDiff = today.getFullYear() - createdAt.getFullYear();
+      let monthDiff = Math.abs(
+        yearDiff * 12 + today.getMonth() - createdAt.getMonth()
       );
-      const thisMonthRevenue = thisMonthOrders.reduce(
-        (total, order) => total + (order.total || 0),
-        0
-      );
-      const revenue = allOrders.reduce(
-        (total, order) => total + (order.total || 0),
-        0
-      );
-      const percentageChange = {
-        users: calculatePercentage(
-          thisMonthUsers.length,
-          previousMonthUsers.length
-        ),
-        products: calculatePercentage(
-          thisMonthProducts.length,
-          previousMonthProducts.length
-        ),
-        orders: calculatePercentage(
-          thisMonthOrders.length,
-          previousMonthOrders.length
-        ),
-        revenue: calculatePercentage(thisMonthRevenue, previousMonthRevenue),
-      };
+      if (monthDiff < 6) {
+        lastSixMonthTransactions[5 - monthDiff] += 1;
+        lastSixMonthRevenue[5 - monthDiff] += order.total || 0;
+      }
+    });
 
-      const count = {
-        revenue,
-        products: productsCount,
-        users: usersCount,
-        orders: allOrders.length,
-      };
+    let inventory: Record<string, number>[] = await getInventory({
+      categories,
+      productsCount,
+    });
+    const userRatio = {
+      male: usersCount - femaleUsersCount,
+      female: femaleUsersCount,
+    };
+    // const modifiedLatestTransaction = latestTransactions.map((i) => ({
+    //   _id: i._id,
+    //   discount: i.discount,
+    //   amount: i.total,
+    //   quantity: i.orderItems.length,
+    //   status: i.status,
+    // }));
 
-      const lastSixMonthTransactions = new Array(6).fill(0);
-      const lastSixMonthRevenue = new Array(6).fill(0);
+    let modifiedLatestTransaction:Array<Object> = [];
+    latestTransactions.forEach((i) => {
+      if (i.orderItems.length > 1) {
+        i.orderItems.forEach((value) => {
+          modifiedLatestTransaction.push({
+            name: value.name,
+            photo: value.photo,
+            amount: value.price,
+            quantity: value.quantity,
+            status: i.status,
+          });
+        });
+      } else {
+        modifiedLatestTransaction.push({
+          name: i.orderItems[0].name,
+          photo: i.orderItems[0].photo,
+          amount: i.orderItems[0].price,
+          quantity: i.orderItems[0].quantity,
+          status: i.status,
+        });
+      }
+    });
 
-      // Getting past six months data for the graph on admin dashboard
-      lastSixMonthOrders.forEach((order) => {
-        let createdAt = order.createdAt;
-        let yearDiff = today.getFullYear() - createdAt.getFullYear();
-        let monthDiff = Math.abs(
-          yearDiff * 12 + today.getMonth() - createdAt.getMonth()
-        );
-        if (monthDiff < 6) {
-          lastSixMonthTransactions[5 - monthDiff] += 1;
-          lastSixMonthRevenue[5 - monthDiff] += order.total || 0;
-        }
-      });
+    stats = {
+      percentageChange,
+      count,
+      inventory,
+      chart: {
+        order: lastSixMonthTransactions,
+        revenue: lastSixMonthRevenue,
+      },
+      userRatio,
+      latestTransactions: modifiedLatestTransaction.slice(0,4),
+    };
 
-      let inventory: Record<string, number>[] = await getInventory({
-        categories,
-        productsCount,
-      });
-      const userRatio = {
-        male: usersCount - femaleUsersCount,
-        female: femaleUsersCount,
-      };
-      const modifiedLatestTransaction = latestTransactions.map((i) => ({
-        _id: i._id,
-        discount: i.discount,
-        amount: i.total,
-        quantity: i.orderItems.length,
-        status: i.status,
-      }));
-
-      stats = {
-        percentageChange,
-        count,
-        inventory,
-        chart: {
-          order: lastSixMonthTransactions,
-          revenue: lastSixMonthRevenue,
-        },
-        userRatio,
-        latestTransactions: modifiedLatestTransaction,
-      };
-
-      cache.set("admin-dashboard-stats", JSON.stringify(stats));
-    }
+    cache.set("admin-dashboard-stats", JSON.stringify(stats));
+    // }
 
     res.status(200).json({
       success: true,
